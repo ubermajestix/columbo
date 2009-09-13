b65b07619863c4d113b97fa4a32c68cdc34f34da
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2009-09-12 18:26:46 -0600

    full set of basic features

--- lib/columbo/app.rb
+++ lib/columbo/app.rb
@@ -10,6 +10,14 @@ module Columbo
     
 
     get '/' do
+      @bookmarklet = ""
+      @local = ['development', nil, 'test'].include?(ENV['RACK_ENV'])
+      File.open("#{Dir.pwd}/lib/columbo/public/js/bookmarklet#{'.localhost' if @local  }.js"){|f| @bookmarklet = f.read}
+      puts "=="*45
+      puts ENV['RACK_ENV']
+      puts ENV.inspect
+      puts @bookmarklet.gsub!("\n\n")
+      puts "=="*45
        erb :index
     end       
   end
